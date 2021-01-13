895d8eb02a562ec13e95cd56a6a9af3c1b9ef072
Ali J. Ghandour <ghandour.aub@gmail.com>
2021-01-13 19:23:46 +0200

    adding evalscript V3 transition support
    adding V3 evalscripts for NDWI, TrueColor and DEM requests

--- src/sh_requests.py
+++ src/sh_requests.py
@@ -4,6 +4,68 @@ from sentinelhub import MimeType, CustomUrlParam, DataSource
 from sentinelhub import SHConfig
 import numpy as np
 from datetime import datetime, timedelta
+
+S2_DEM_SCRIPT_V3 = """
+    //VERSION=3
+    function setup() {
+        return {
+            input: [{
+                bands: ["DEM"],
+            }],
+            output: {
+                bands: 1,
+                sampleType: "FLOAT32"
+            }
+        };
+    }
+
+    function evaluatePixel(sample) {
+        return [sample.DEM];
+    }
+"""
+
+S2_NDWI_SCRIPT_V3 = """
+    //VERSION=3
+    function setup() {
+        return {
+            input: [{
+                bands: ["B03", "B08", "dataMask"],
+            }],
+            output: {
+                bands: 3,
+                sampleType: "FLOAT32"
+            }
+        };
+    }
+
+    function evaluatePixel(sample) {
+        return [sample.B03,
+                sample.B08,
+				sample.dataMask];
+    }
+"""
+
+S2_TRUECOLOR_SCRIPT_V3 = """
+    //VERSION=3
+    function setup() {
+        return {
+            input: [{
+                bands: ["B02", "B03", "B04", "dataMask"],
+            }],
+            output: {
+                bands: 4,
+                sampleType: "FLOAT32"
+            }
+        };
+    }
+
+    function evaluatePixel(sample) {
+        return [2.*sample.B04,
+				2.*sample.B03,
+                2.*sample.B02,
+				sample.dataMask];
+    }
+"""
     
 def get_S2_request(layer, dam_bbox, date, resx, resy, maxcc):
     return WcsRequest(data_folder=None,
@@ -14,7 +76,7 @@ def get_S2_request(layer, dam_bbox, date, resx, resy, maxcc):
                       image_format=MimeType.TIFF_d32f,
                       maxcc=maxcc,
                       time_difference=timedelta(hours=2),
-                      custom_url_params={CustomUrlParam.SHOWLOGO: False})
+                      custom_url_params={CustomUrlParam.EVALSCRIPT: S2_NDWI_SCRIPT_V3 if layer == 'NDWI' else S2_TRUECOLOR_SCRIPT_V3, CustomUrlParam.SHOWLOGO: False})
 
 def get_S2_wmsrequest(layer, dam_bbox, date, width, height, maxcc):
     return WmsRequest(data_folder=None,
@@ -40,5 +102,4 @@ def get_DEM_request(dam_bbox, resx, resy):
                       resx=f'{resx}m', resy=f'{resy}m',
                       bbox=dam_bbox,
                       image_format=MimeType.TIFF_d32f,
-                      custom_url_params={CustomUrlParam.SHOWLOGO: False})
-
+                      custom_url_params={CustomUrlParam.EVALSCRIPT: S2_DEM_SCRIPT_V3, CustomUrlParam.SHOWLOGO: False})
